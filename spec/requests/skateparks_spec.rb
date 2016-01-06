3e4e5f58757d99737c7a8bfbceca9000562cf2e1
Chris Scott <christo247@gmail.com>
2016-01-05 20:40:40 -0800

    Rewrite specs for better readability

--- spec/requests/skateparks_spec.rb
+++ spec/requests/skateparks_spec.rb
@@ -1,59 +1,62 @@
 require 'rails_helper'
 
-RSpec.describe 'Skateparks' do
-  context 'GET /skateparks' do
-    it 'returns a list of all skateparks' do
-      skateparks = create_skateparks
+RSpec.describe 'GET /skateparks' do
+  it 'returns a list of all skateparks' do
+    skateparks = [
+      create(:skatepark), create(:skatepark, :other)]
 
-      get '/skateparks'
+    get '/skateparks'
 
-      skateparks.each_with_index do |park, i|
-        expect(json_body[i]['id']).to eq(park.id)
-        expect(json_body[i]['name']).to eq(park.name)
-        expect(json_body[i]['address']).to eq(park.address)
-      end
-    end
-
-    it 'returns users that have favorited skateparks' do
-      user = create_user
-      create_skateparks.each { |park| park.users << user }
-
-      get '/skateparks'
-
-      expect(json_body[0]['users'][0]['name']).to eq(user.name)
-      expect(json_body[1]['users'][0]['email']).to eq(user.email)
+    skateparks.each_with_index do |skatepark, i|
+      expect(json_body[i]['id']).to eq(skatepark.id)
+      expect(json_body[i]['name']).to eq(skatepark.name)
+      expect(json_body[i]['address']).to eq(skatepark.address)
     end
   end
 
-  context 'GET /skateparks/:id' do
-    it 'returns skatepark with proper id' do
-      other = create_skateparks[-1]
+  it 'returns users that have favorited skateparks' do
+    user = create(:user)
+    skateparks = [
+      create(:skatepark), create(:skatepark, :other)]
+    skateparks.each { |park| park.users << user }
 
-      get "/skateparks/#{other.id}"
+    get '/skateparks'
 
-      expect(json_body['id']).to eq(other.id)
-      expect(json_body['name']).to eq(other.name)
-      expect(json_body['address']).to eq(other.address)
-      expect(json_body['state']).to eq(other.state)
-      expect(json_body['lat']).to eq(other.lat)
-      expect(json_body['lon']).to eq(other.lon)
-    end
+    expect(json_body[0]['users'][0]['name']).to eq(user.name)
+    expect(json_body[1]['users'][0]['email']).to eq(user.email)
+  end
+end
 
-    it 'returns users that have favorited skatepark' do
-      park = create_skatepark
-      user = create_user
-      park.users << user
+RSpec.describe 'GET /skateparks/:id' do
+  it 'returns skatepark with proper id' do
+    skateparks = [
+      create(:skatepark), create(:skatepark, :other)]
+    other = skateparks[-1]
 
-      get "/skateparks/#{park.id}"
+    get "/skateparks/#{other.id}"
 
-      expect(json_body['users'][0]['name']).to eq(user.name)
-      expect(json_body['users'][0]['email']).to eq(user.email)
-    end
+    expect(json_body['id']).to eq(other.id)
+    expect(json_body['name']).to eq(other.name)
+    expect(json_body['address']).to eq(other.address)
+    expect(json_body['state']).to eq(other.state)
+    expect(json_body['lat']).to eq(other.lat)
+    expect(json_body['lon']).to eq(other.lon)
+  end
 
-    it 'returns a 404 if skatepark with id is not found' do
-      get '/skateparks/420'
+  it 'returns users that have favorited skatepark' do
+    skatepark = create(:skatepark)
+    user = create(:user)
+    skatepark.users << user
 
-      expect(response.status).to eq(404)
-    end
+    get "/skateparks/#{skatepark.id}"
+
+    expect(json_body['users'][0]['name']).to eq(user.name)
+    expect(json_body['users'][0]['email']).to eq(user.email)
+  end
+
+  it 'returns a 404 if skatepark with id is not found' do
+    get '/skateparks/420'
+
+    expect(response.status).to eq(404)
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,97 +1,98 @@
 require 'rails_helper'
 
-RSpec.describe 'Users' do
-  let(:users) { create_users }
-  let(:park) { create_skatepark }
-  let(:user) { create_user }
+RSpec.describe 'GET /users' do
+  it 'should return a list of all users' do
+    users = [create(:user), create(:user, :other)]
 
-  context 'GET /users' do
-    it 'should return a list of all users' do
-      created_users = users
+    get '/users'
 
-      get '/users'
-
-      created_users.each_with_index do |user, i|
-        expect(json_body[i]['id']).to eq(user.id)
-        expect(json_body[i]['email']).to eq(user.email)
-        expect(json_body[i]['name']).to eq(user.name)
-      end
-    end
-
-    it 'should return skateparks favorites by users' do
-      users.each { |user| user.skateparks << park }
-
-      get '/users'
-
-      json_body.each do |user|
-        skatepark = user['skateparks'][0]
-        expect(skatepark['name']).to eq(park.name)
-        expect(skatepark['address']).to eq(park.address)
-      end
+    users.each_with_index do |user, i|
+      expect(json_body[i]['id']).to eq(user.id)
+      expect(json_body[i]['email']).to eq(user.email)
+      expect(json_body[i]['name']).to eq(user.name)
     end
   end
 
-  context 'GET /users/:id' do
-    it 'should return user with the corresponding :id' do
-      get "/users/#{user.id}"
+  it 'should return skateparks favorites by users' do
+    users = [create(:user), create(:user, :other)]
+    skatepark = create(:skatepark)
+    users.each { |user| user.skateparks << skatepark }
 
-      expect(json_body['id']).to eq(user.id)
-      expect(json_body['name']).to eq(user.name)
-      expect(json_body['email']).to eq(user.email)
-    end
+    get '/users'
 
-    it 'should return skateparks that user has favorited' do
-      user.skateparks << park
-
-      get "/users/#{user.id}"
-
-      expect(json_body['skateparks'][0]['name']).to eq(park.name)
-      expect(json_body['skateparks'][0]['address']).to eq(park.address)
-    end
-
-    it 'should return 404 if user cannot be found' do
-      get '/users/420'
-
-      expect(response.status).to eq(404)
+    json_body.each do |user|
+      park = user['skateparks'][0]
+      expect(park['name']).to eq(skatepark.name)
+      expect(park['address']).to eq(skatepark.address)
     end
   end
+end
 
-  context 'POST /users' do
-    it 'should create and return a user' do
-      user = build(:user)
+RSpec.describe 'GET /users/:id' do
+  it 'should return user with the corresponding :id' do
+    user = create(:user)
 
-      expect do
-        post '/users', user: user.to_json
-      end.to change { User.count }.by(1)
-      expect(json_body['name']).to eq(user.name)
-      expect(json_body['email']).to eq(user.email)
-    end
+    get "/users/#{user.id}"
 
-    it 'should return 400 if user cannot be created' do
-      user = build(:user, :invalid)
-
-      expect do
-        post '/users', user: user.to_json
-      end.to_not change { User.count }
-      expect(response.status).to eq(400)
-    end
+    expect(json_body['id']).to eq(user.id)
+    expect(json_body['name']).to eq(user.name)
+    expect(json_body['email']).to eq(user.email)
   end
 
-  context 'DELETE /users' do
-    it 'should destroy user with the corresponding :id' do
-      user = create(:user)
+  it 'should return skateparks that user has favorited' do
+    user = create(:user)
+    skatepark = create(:skatepark)
+    user.skateparks << skatepark
 
-      expect do
-        delete "/users/#{user.id}"
-      end.to change { User.count }.by(-1)
-      expect(response.status).to eq(204)
-    end
+    get "/users/#{user.id}"
 
-    it 'should return 400 if user does not exist' do
-      expect do
-        delete '/users/420'
-      end.to_not change { User.count }
-      expect(response.status).to eq(400)
-    end
+    park = json_body['skateparks'][0]
+    expect(park['name']).to eq(skatepark.name)
+    expect(park['address']).to eq(skatepark.address)
+  end
+
+  it 'should return 404 if user cannot be found' do
+    get '/users/420'
+
+    expect(response.status).to eq(404)
+  end
+end
+
+RSpec.describe 'POST /users' do
+  it 'should create and return a user' do
+    user = build(:user)
+
+    expect do
+      post '/users', user: user.to_json
+    end.to change { User.count }.by(1)
+    expect(json_body['name']).to eq(user.name)
+    expect(json_body['email']).to eq(user.email)
+  end
+
+  it 'should return 400 if user cannot be created' do
+    user = build(:user, :invalid)
+
+    expect do
+      post '/users', user: user.to_json
+    end.to_not change { User.count }
+    expect(response.status).to eq(400)
+  end
+end
+
+RSpec.describe 'DELETE /users' do
+  it 'should destroy user with the corresponding :id' do
+    user = create(:user)
+
+    expect do
+      delete "/users/#{user.id}"
+    end.to change { User.count }.by(-1)
+    expect(response.status).to eq(204)
+  end
+
+  it 'should return 400 if user does not exist' do
+    expect do
+      delete '/users/420'
+    end.to_not change { User.count }
+    expect(response.status).to eq(400)
   end
 end
