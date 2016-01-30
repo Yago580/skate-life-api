c1758556cbdf31a2915b99133ecec12c7530c696
Chris Scott <christo247@gmail.com>
2016-01-29 19:49:22 -0800

    Remove unneeded factory traits

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,24 +6,12 @@ FactoryGirl.define do
     lat 37.632840
     lon(-122.064482)
     users_who_faved []
-
-    trait :other do
-      name 'Other'
-      address '555 Other Way Othertown CO 12345'
-      lat 56.567234
-      lon(-122.064482)
-    end
   end
 
   factory :user do
     sequence(:name) { |n| "person#{n}" }
     sequence(:email) { |n| "swag#{n}@swag.swag" }
     favorite_parks []
-
-    trait :other do
-      name 'swaggy'
-      email 'swaggy@swaggy.swaggy'
-    end
 
     trait :invalid do
       name 'swag'
@@ -32,7 +20,7 @@ FactoryGirl.define do
   end
 
   factory :favorite do
-    sequence(:user_id)
-    sequence(:skatepark_id)
+    user_id Integer
+    skatepark_id Integer
   end
 end
