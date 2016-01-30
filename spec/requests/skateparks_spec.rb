6bbbb0d4c7ec14058fce4bea02281a2a25edc9b0
Chris Scott <christo247@gmail.com>
2016-01-29 19:36:31 -0800

    Refactor skatepark request spec

--- spec/requests/skateparks_spec.rb
+++ spec/requests/skateparks_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'GET /skateparks' do
   it 'returns a list of all skateparks' do
-    skateparks = [
-      create(:skatepark), create(:skatepark, :other)]
+    skateparks = create_list(:skatepark, 2)
 
-    get '/skateparks'
+    get skateparks_path
 
     skateparks.each_with_index do |skatepark, i|
       expect(json_body[i]['id']).to eq(skatepark.id)
@@ -17,26 +16,24 @@ end
 
 RSpec.describe 'GET /skateparks/:id' do
   it 'returns skatepark with proper id' do
-    skateparks = [
-      create(:skatepark), create(:skatepark, :other)]
-    other = skateparks[-1]
+    skatepark = create(:skatepark)
 
-    get "/skateparks/#{other.id}"
+    get skatepark_path skatepark
 
-    expect(json_body['id']).to eq(other.id)
-    expect(json_body['name']).to eq(other.name)
-    expect(json_body['address']).to eq(other.address)
-    expect(json_body['state']).to eq(other.state)
-    expect(json_body['lat']).to eq(other.lat)
-    expect(json_body['lon']).to eq(other.lon)
+    expect(json_body['id']).to eq(skatepark.id)
+    expect(json_body['name']).to eq(skatepark.name)
+    expect(json_body['address']).to eq(skatepark.address)
+    expect(json_body['state']).to eq(skatepark.state)
+    expect(json_body['lat']).to eq(skatepark.lat)
+    expect(json_body['lon']).to eq(skatepark.lon)
   end
 
   it 'returns users that have favorited skatepark' do
     skatepark = create(:skatepark)
     user = create(:user)
-    create_favorite(user.id, skatepark.id)
+    create(:favorite, user_id: user.id, skatepark_id: skatepark.id)
 
-    get "/skateparks/#{skatepark.id}"
+    get skatepark_path skatepark
 
     users_who_faved = json_body['users_who_faved']
     expect(users_who_faved[0]['name']).to eq(user.name)
