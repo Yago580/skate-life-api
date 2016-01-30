f54782a7ef386bdb1ede6d7a4c5eb3edc12a879a
Chris Scott <christo247@gmail.com>
2016-01-29 19:58:18 -0800

    Refactor favorites spec

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -5,24 +5,19 @@ RSpec.describe 'POST /favorites' do
     user = create(:user)
     skatepark = create(:skatepark)
 
-    expect do
-      post '/favorites', user_id: user.id, skatepark_id: skatepark.id
-    end.to change { user.favorite_parks.count }.by(1)
+    post favorites_path, user_id: user.id, skatepark_id: skatepark.id
 
-    fav = Favorite.where(user_id: user.id, skatepark_id: skatepark.id).first
-
-    expect(fav.user).to eq(user)
-    expect(fav.skatepark).to eq(skatepark)
+    expect(Favorite.last.user).to eq(user)
+    expect(Favorite.last.skatepark).to eq(skatepark)
   end
 
   it 'should return 422 if favorite already exists' do
     user = create(:user)
     skatepark = create(:skatepark)
-    user.favorite_parks << skatepark
+    create(:favorite, user_id: user.id, skatepark_id: skatepark.id)
 
-    expect do
-      post '/favorites', user_id: user.id, skatepark_id: skatepark.id
-    end.to_not change { user.favorite_parks.count }
+    post favorites_path, user_id: user.id, skatepark_id: skatepark.id
+
     expect(response.status).to eq(422)
   end
 end
@@ -31,11 +26,10 @@ RSpec.describe 'DELETE /favorites' do
   it "should remove park from user's favorites" do
     user = create(:user)
     skatepark = create(:skatepark)
-    user.favorite_parks << skatepark
+    create(:favorite, user_id: user.id, skatepark_id: skatepark.id)
 
-    expect do
-      delete "/favorite/#{user.id}/#{skatepark.id}"
-    end.to change { user.favorite_parks.count }.by(-1)
+    delete "/favorite/#{user.id}/#{skatepark.id}"
+
     expect(response.status).to eq(204)
   end
 end
